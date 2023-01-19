8794cb220fead38bea487a2f06e46a58e8866f79
Raphael <okolonnaemeka2017@gmail.com>
2023-01-19 08:58:51 -0500

    Create refresh variable.

--- src/modules/variables.js
+++ src/modules/variables.js
@@ -1,5 +1,6 @@
 const listContainer = document.querySelector('.container');
 const textInput = document.querySelector('.the-input');
 const clearBtn = document.querySelector('.clear');
+const refresh = document.querySelector('.fa-rotate');
 
-export { listContainer, textInput, clearBtn };
+export { listContainer, textInput, clearBtn, refresh };
